ae790d6f1dbba0854b74d9364149250e5e6c32ba
Mark <super16mail@gmail.com>
2022-01-08 15:28:19 +0300

    Create Country model and refine view for countries page

--- django_alpine_tailwindcss/tables/models.py
+++ django_alpine_tailwindcss/tables/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Country(models.Model):
+    title = models.CharField(max_length=50, unique=True)
+    capital = models.CharField(max_length=50, unique=True)
+    population = models.IntegerField()
+
+    class Meta:
+        verbose_name_plural = 'countries'
+
+    def __str__(self):
+        return self.title
--- django_alpine_tailwindcss/tables/views.py
+++ django_alpine_tailwindcss/tables/views.py
@@ -1,9 +1,20 @@
+from django.http import JsonResponse
 from django.views.generic.list import ListView
 from django.views.generic import TemplateView
 
+from .models import Country
+
 
 class CountriesListView(ListView):
+    model = Country
     template_name = 'countries.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['object_list'] = JsonResponse(
+            list(self.object_list.values()), safe=False
+        ).content.decode('utf-8')
+        return context
 
 
 class IndexView(TemplateView):
