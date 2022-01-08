29bab447e171fdc3625d3596b0d67ea389cf4854
Mark <super16mail@gmail.com>
2022-01-08 15:23:00 +0300

    Create HTML templates and add them to Webpack

--- django_alpine_tailwindcss/boilerplates/webpack.config.js
+++ django_alpine_tailwindcss/boilerplates/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'production',
@@ -7,6 +8,18 @@ module.exports = {
   plugins: [
     new MiniCssExtractPlugin({
       filename: 'style.css',
+    }),
+    new HtmlWebpackPlugin({
+      filename: '../templates/base.html',
+      template: 'src/base.html',
+      inject: false,
+      minify: false,
+    }),
+    new HtmlWebpackPlugin({
+      filename: '../templates/index.html',
+      template: 'src/index.html',
+      inject: false,
+      minify: false,
     }),
   ],
   module: {
