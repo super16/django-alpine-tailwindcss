decaa965b301aad776605c99f05f1084f19e8467
Mark <super16mail@gmail.com>
2022-01-08 15:08:35 +0300

    Install Tailwind CSS

--- django_alpine_tailwindcss/boilerplates/src/index.js
+++ django_alpine_tailwindcss/boilerplates/src/index.js
@@ -1,3 +1,4 @@
+import './style.css';
 import Alpine from 'alpinejs';
 
 Alpine.data('app', () => ({
---
+++ django_alpine_tailwindcss/boilerplates/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    './src/*.html',
+    './src/*.js'
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
